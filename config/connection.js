ec3a571c6e2d8eafd84f1fdfb0d997e2ee55c21a
Alanto Manu <alantomanu501@gmail.com>
2024-07-17 09:45:06 +0530

    Update connection.js to use environment variables and add .env to .gitignore

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
-const mongoClient = require('mongodb').MongoClient
+const mongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
 const state = {
   db: null
-}
+};
+
 module.exports.connect = function (done) {
-  const url = 'mongodb://127.0.0.1:27017/'
-  const dbname = 'shopping'
+  const url = process.env.MONGODB_URL;
+  const dbname = process.env.DB_NAME;
+
   mongoClient.connect(url, { useUnifiedTopology: true }, (err, data) => {
-    if (err) return done(err)
-    state.db = data.db(dbname)
-    done()
-  })
-}
+    if (err) return done(err);
+    state.db = data.db(dbname);
+    done();
+  });
+};
+
 module.exports.get = function () {
   return state.db;
 };
